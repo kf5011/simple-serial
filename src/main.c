f4cd29260f7d38131bcf7a6f920760aa795d8092
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2017-07-04 20:34:26 +0100

    Tidied output

--- src/main.c
+++ src/main.c
@@ -2,14 +2,18 @@
     View in the serial monitor
 */
 #include <stdio.h>
+#include <stdint.h>
+
+#include <mbed_wait_api.h>
 
 int main()
 {
     while(1) {
         int n;
-        printf("Hello world\n");
+        printf("Hello world\n\u269b\n");
         for( n=10 ; n>0 ; n-- ) {
-            printf("%3d ..",n);
+            printf("%3d …",n);
+            wait(0.50);
         }
         putchar('\n');
     }
